465a39f73d84cba6fd29905047fd3d2055808eb6
David Walker <david.walker5@icloud.com>
2020-04-13 20:27:48 +0100

    Simplify entity definitions

--- src/FlightRecorder.Entities/Db/Aircraft.cs
+++ src/FlightRecorder.Entities/Db/Aircraft.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace FlightRecorder.Entities.Db
@@ -7,11 +6,6 @@ namespace FlightRecorder.Entities.Db
     [ExcludeFromCodeCoverage]
     public partial class Aircraft
     {
-        public Aircraft()
-        {
-            Sighting = new HashSet<Sighting>();
-        }
-
         [Key]
         public long Id { get; set; }
         public long ModelId { get; set; }
@@ -20,6 +14,5 @@ namespace FlightRecorder.Entities.Db
         public long Manufactured { get; set; }
 
         public virtual Model Model { get; set; }
-        public virtual ICollection<Sighting> Sighting { get; set; }
     }
 }
--- src/FlightRecorder.Entities/Db/Airline.cs
+++ src/FlightRecorder.Entities/Db/Airline.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace FlightRecorder.Entities.Db
@@ -7,15 +6,8 @@ namespace FlightRecorder.Entities.Db
     [ExcludeFromCodeCoverage]
     public partial class Airline
     {
-        public Airline()
-        {
-            Flight = new HashSet<Flight>();
-        }
-
         [Key]
         public long Id { get; set; }
         public string Name { get; set; }
-
-        public virtual ICollection<Flight> Flight { get; set; }
     }
 }
