d4e20ea69f998f88558f15e863682327cd4d0eba
David Walker <dwalker@cellmark.co.uk>
2020-04-13 10:06:25 +0100

    Added EF migrations

---
+++ src/FlightRecorder.Data/Migrations/20200413084833_security.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlightRecorder.Data.Migrations
+{
+    public partial class security : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "USER",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Password = table.Column<string>(type: "VARCHAR(1000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_USER", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "USER");
+        }
+    }
+}
