17d42f3ead1ea27b064caf4d4e32f9ea00973b15
Dave Walker <david.walker5@icloud.com>
2023-12-11 14:54:38 +0000

    Fixup flight editing interaction with IATA Code validation

--- src/FlightRecorder.Mvc/Attributes/IATACodeAttribute.cs
+++ src/FlightRecorder.Mvc/Attributes/IATACodeAttribute.cs
@@ -1,9 +1,5 @@
 ﻿using FlightRecorder.Mvc.Api;
-using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using FlightRecorder.Mvc.Models;
 
 namespace FlightRecorder.Mvc.Attributes
 {
@@ -14,17 +10,22 @@ namespace FlightRecorder.Mvc.Attributes
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            FlightDetailsViewModel model = (FlightDetailsViewModel)validationContext.ObjectInstance;
-            List<string> codes = _client.GetAirportsAsync(1, int.MaxValue).Result.Select(x => x.Code).ToList();
+            // Get a list of airport IATA codes to validate against
+            var codes = _client.GetAirportsAsync(1, int.MaxValue).Result.Select(x => x.Code).ToList();
 
-            if ((validationContext.MemberName == "Embarkation") && !codes.Contains(model.Embarkation.ToUpper()))
-            {
-                return new ValidationResult($"{model.Embarkation} is not a valid IATA code for the point of embarkation");
-            }
+            // Get the properties of the object instance being validated
+            var properties = validationContext.ObjectInstance.GetType().GetProperties();
 
-            if ((validationContext.MemberName == "Destination") && !codes.Contains(model.Destination.ToUpper()))
+            // See if the object has the named property (at this point, it should have)
+            var property = properties.FirstOrDefault(x => x.Name == validationContext.MemberName);
+            if (property != null)
             {
-                return new ValidationResult($"{model.Destination} is not a valid IATA code for the destination");
+                // It does, so get the value and check it's in the code list
+                var propertyValue = ((string)property.GetValue(validationContext.ObjectInstance) ?? "").ToUpper();
+                if (!codes.Contains(propertyValue))
+                {
+                    return new ValidationResult($"{propertyValue} is not a valid IATA code for {validationContext.MemberName}");
+                }
             }
 
             return ValidationResult.Success;
