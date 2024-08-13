371576738ceee699b996525b82dd41623e021c16
Dave Walker <david.walker5@icloud.com>
2024-08-13 09:48:43 +0100

    Added sighting statistics report to the UI

---
+++ src/FlightRecorder.Mvc/Controllers/SightingStatisticsController.cs
@@ -0,0 +1,34 @@
+﻿using FlightRecorder.Mvc.Api;
+using FlightRecorder.Mvc.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace FlightRecorder.Mvc.Controllers
+{
+    [Authorize]
+    public class SightingStatisticsController : Controller
+    {
+        private readonly ReportsClient _reportsClient;
+        private readonly IOptions<AppSettings> _settings;
+
+        public SightingStatisticsController(
+            ReportsClient reportsClient,
+            IOptions<AppSettings> settings)
+        {
+            _reportsClient = reportsClient;
+            _settings = settings;
+        }
+
+        /// <summary>
+        /// Retrieve and serve the report
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var report = await _reportsClient.SightingStatisticsAsync();
+            return View(report);
+        }
+    }
+}
---
+++ src/FlightRecorder.Mvc/Entities/SightingStatistics.cs
@@ -0,0 +1,13 @@
+﻿namespace FlightRecorder.Mvc.Entities
+{
+    public record SightingStatistics
+    (
+        int Aircraft,
+        int Manufacturers,
+        int Models,
+        int Airlines,
+        int Flights,
+        int Sightings,
+        int Locations
+    );
+}
